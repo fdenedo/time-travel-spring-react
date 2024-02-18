7be5643d5c9c59b006934135b6b852be22a01533
Fure Denedo <adenedo@gmail.com>
2024-02-18 17:09:14 +0000

    updated TimeMachineRepository to return single Time Machine from findByMakeAndModel()

--- src/main/java/com/example/timetravelbooks/ttsp/TimeMachineRepository.java
+++ src/main/java/com/example/timetravelbooks/ttsp/TimeMachineRepository.java
@@ -2,10 +2,8 @@ package com.example.timetravelbooks.ttsp;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface TimeMachineRepository extends MongoRepository<TimeMachineEntity, String> {
 
-    List<TimeMachineEntity> findByMakeAndModel(String make, String model);
+    TimeMachineEntity findByMakeAndModel(String make, String model);
 
 }
