079736479b176b3589c499078008d88c4cabb8f8
Fure Denedo <adenedo@gmail.com>
2024-02-18 17:11:47 +0000

    updated TimeTravelServiceInitializer to use csv file, added csv file for services

--- src/main/java/com/example/timetravelbooks/ttsp/TimeTravelServiceInitializer.java
+++ src/main/java/com/example/timetravelbooks/ttsp/TimeTravelServiceInitializer.java
@@ -1,11 +1,13 @@
 package com.example.timetravelbooks.ttsp;
 
-import com.example.timetravelbooks.model.DateRange;
+import com.opencsv.bean.CsvToBeanBuilder;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,62 +38,20 @@ public class TimeTravelServiceInitializer {
     }
 
     private List<TimeTravelServiceEntity> createTimeTravelServices() {
-        List<TimeTravelServiceEntity> services = new ArrayList<>();
-
-        // SERVICE 1
-        List<DateRange> s1ranges = new ArrayList<>();
-        s1ranges.add(new DateRange(
-                LocalDate.of(-10000, 1, 1),
-                LocalDate.of(1850, 1, 1)
-        ));
-
-        TimeTravelServiceEntity service1 = new TimeTravelServiceEntity(
-                "1",
-                ttspRepository.findByName("FirstTravel"),
-                timeMachineRepository.findByMakeAndModel("QuantumCorp", "QM-2000").get(0),
-                2,
-                s1ranges,
-                1000000.0,
-                3000000.0
-        );
-        services.add(service1);
-
-        // SERVICE 2
-        List<DateRange> s2ranges = new ArrayList<>();
-        s2ranges.add(new DateRange(
-                LocalDate.of(-3000, 1, 1),
-                LocalDate.of(100, 1, 1)
-        ));
-
-        TimeTravelServiceEntity service2 = new TimeTravelServiceEntity(
-                "2",
-                ttspRepository.findByName("FirstTravel"),
-                timeMachineRepository.findByMakeAndModel("QuantumCorp", "QM-4000").get(0),
-                4,
-                s2ranges,
-                1000000.0,
-                3000000.0
-        );
-        services.add(service2);
-
-        // SERVICE 3
-        List<DateRange> s3ranges = new ArrayList<>();
-        s3ranges.add(new DateRange(
-                LocalDate.of(0, 1, 1),
-                LocalDate.of(2024, 2, 16)
-        ));
-
-        TimeTravelServiceEntity service3 = new TimeTravelServiceEntity(
-                "3",
-                ttspRepository.findByName("Rewind"),
-                timeMachineRepository.findByMakeAndModel("QuantumCorp", "QM-3000").get(0),
-                2,
-                s3ranges,
-                800000.0,
-                3200000.0
-        );
-        services.add(service3);
-
-        return services;
+        try (InputStream servicesFileAsStream = getClass().getResourceAsStream("/static/time-travel-services.csv")) {
+            if (servicesFileAsStream == null) throw new NullPointerException("Could not find CSV file for services");
+            Reader reader = new InputStreamReader(servicesFileAsStream);
+            return new CsvToBeanBuilder<TimeTravelServiceCsvRepr>(reader)
+                    .withType(TimeTravelServiceCsvRepr.class)
+                    .build()
+                    .parse()
+                    .stream().map(service -> service.toEntity(this.timeMachineRepository, this.ttspRepository))
+                    .toList();
+        } catch (Exception e) {
+            System.out.println("Error reading CSV file for time travel services:");
+            // TODO: Look into adding more robust logging
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 }
