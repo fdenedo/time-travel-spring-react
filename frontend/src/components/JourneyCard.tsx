d430687d9f1760a5bf93927606892b80d714e36e
Fure Denedo <adenedo@gmail.com>
2024-02-19 14:47:56 +0000

    updated JourneyCard.tsx to see initial return journey card styling

--- frontend/src/components/JourneyCard.tsx
+++ frontend/src/components/JourneyCard.tsx
@@ -17,6 +17,7 @@ interface JourneyProps {
 }
 
 const JourneyCard = ({ journey }: JourneyProps) => {
+  const isReturnJourney = journey.targetReturnDate !== null || undefined;
 
   const [, dMonth, dDay, dYear] = new Date(journey.departureDate).toDateString().split(' ');
   const dTime = journey.departureTime.split(':').slice(0, -1).join(':');
@@ -39,30 +40,55 @@ const JourneyCard = ({ journey }: JourneyProps) => {
   
   return (
       <Card className="w-[60rem] my-8">
-        <CardContent className="p-2">
-          <div className="p-4 flex flex-row justify-between items-top">
-            <div className="provider-section flex flex-col justify-center">
-              <h2>{journey.providerName}</h2>
-            </div>
-            <div className="time-section flex flex-row">
-              <div className="journey-from flex flex-col justify-start">
-                <p className="text-slate-400">{`${dDay} ${dMonth}`}</p>
-                <p className="text-2xl">{dYear}</p>
-                <div className="rounded-lg bg-slate-400 p-[1px] mt-2">
-                  <p className="text-amber-50">{dTime}</p>
+        <CardContent className="p-2 flex flex-row">
+          <div className="journey-details flex flex-grow flex-col justify-between">
+            <div className="first-journey-details p-4 flex flex-row justify-between items-top">
+              <div className="provider-section flex flex-col justify-center">
+                <h2>{journey.providerName}</h2>
+              </div>
+              <div className="time-section flex flex-row">
+                <div className="journey-from flex flex-col justify-start">
+                  <p className="text-slate-400">{`${dDay} ${dMonth}`}</p>
+                  <p className="text-2xl">{dYear}</p>
+                  <div className="rounded-lg bg-slate-400 p-[1px] mt-2">
+                    <p className="text-amber-50">{dTime}</p>
+                  </div>
+                </div>
+                <div className="separator flex flex-col justify-center items-center mx-6">
+                  <div className="h-[2px] w-32 bg-slate-600"></div>
+                </div>
+                <div className="journey-to flex flex-col justify-start">
+                  <p className="text-slate-400">{`${aDay} ${aMonth}`}</p>
+                  <p className="text-2xl">{aYear}</p>
                 </div>
               </div>
-              <div className="separator flex flex-col justify-center items-center mx-6">
-                <div className="h-[2px] w-32 bg-slate-600"></div>
-              </div>
-              <div className="journey-to flex flex-col justify-start">
-                <p className="text-slate-400">{`${aDay} ${aMonth}`}</p>
-                <p className="text-2xl">{aYear}</p>
-              </div>
             </div>
-            <div className="price border-l px-12 flex flex-col justify-center">
-              <p className="text-xl"><span>£</span>{formatPrice(journey.totalPrice)}</p>
-            </div>
+            {isReturnJourney && 
+              <div className="return-journey-details p-4 flex flex-row justify-between items-top">
+                <div className="provider-section flex flex-col justify-center">
+                  <h2>{journey.providerName}</h2>
+                </div>
+                <div className="time-section flex flex-row">
+                  <div className="journey-from flex flex-col justify-start">
+                    <p className="text-slate-400">{`${dDay} ${dMonth}`}</p>
+                    <p className="text-2xl">{dYear}</p>
+                    <div className="rounded-lg bg-slate-400 p-[1px] mt-2">
+                      <p className="text-amber-50">{dTime}</p>
+                    </div>
+                  </div>
+                  <div className="separator flex flex-col justify-center items-center mx-6">
+                    <div className="h-[2px] w-32 bg-slate-600"></div>
+                  </div>
+                  <div className="journey-to flex flex-col justify-start">
+                    <p className="text-slate-400">{`${aDay} ${aMonth}`}</p>
+                    <p className="text-2xl">{aYear}</p>
+                  </div>
+                </div>
+              </div>
+            }
+          </div>
+          <div className="price border-l px-12 flex flex-col justify-center">
+            <p className="text-xl"><span>£</span>{formatPrice(journey.totalPrice)}</p>
           </div>
         </CardContent>
         <CardFooter className="bg-slate-100 flex flex-row items-center p-2 pl-6">
