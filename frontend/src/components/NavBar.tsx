5c26eb9acd786d6753335462256f925e297b1b7f
Fure Denedo <adenedo@gmail.com>
2024-02-22 15:50:17 +0000

    added logo div to nav bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="navbar flex flex-row justify-end space-x-10">
-            <li>
-                <Link to={"/"}>Home</Link>
-            </li>
-            <li>
-                <Link to={"/journeys"}>Journeys</Link>
-            </li>
-            <li>
-                <Link to={"/journeys/search"}>Search</Link>
-            </li>
+        <nav className="navbar flex flex-row list-none p-2">
+            <div className="home-logo">
+                <Link to={"/"}>Time Travel Company</Link>
+            </div>
+            <div className="nav-links flex flex-row flex-grow justify-end space-x-10 list-none">
+                <li>
+                    <Link to={"/journeys"}>Journeys</Link>
+                </li>
+                <li>
+                    <Link to={"/journeys/search"}>Search</Link>
+                </li>
+            </div>
         </nav>
     );
 }
