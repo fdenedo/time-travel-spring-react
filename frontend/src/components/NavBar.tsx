c25a301154374a62ca4ea050f512716ed16bcabf
Fure Denedo <adenedo@gmail.com>
2024-02-22 16:38:41 +0000

    added placeholder for sign up and login links

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
     return (
@@ -6,12 +7,18 @@ export default function Navbar() {
             <div className="home-logo">
                 <Link to={"/"}>Time Travel Company</Link>
             </div>
-            <div className="nav-links flex flex-row flex-grow justify-end space-x-10 list-none">
+            <div className="nav-links flex flex-row flex-grow justify-end items-center space-x-10 list-none">
                 <li>
                     <Link to={"/journeys"}>Journeys</Link>
                 </li>
                 <li>
                     <Link to={"/journeys/search"}>Search</Link>
+                </li>
+                <li>
+                    Sign Up
+                </li>
+                <li>
+                    <Button>Log In</Button>
                 </li>
             </div>
         </nav>
