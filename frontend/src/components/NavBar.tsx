486fa5138780180acea6dbbb8f18a7640c39fafd
Fure Denedo <adenedo@gmail.com>
2024-02-27 10:43:44 +0000

    placed nav links in unordered list tag

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,22 +7,24 @@ export default function Navbar() {
             <div className="home-logo">
                 <Link to={"/"}>Time Travel Company</Link>
             </div>
-            <div className="nav-links flex flex-row flex-grow justify-end items-center space-x-10 list-none">
-                <li>
-                    <Link to={"/journeys"}>Journeys</Link>
-                </li>
-                <li>
-                    <Link to={"/journeys/search"}>Search</Link>
-                </li>
-                <li>
-                    <Link to={"/journeys/982573"}>(Details)</Link>
-                </li>
-                <li>
-                    Sign Up
-                </li>
-                <li>
-                    <Button>Log In</Button>
-                </li>
+            <div className="nav-links flex flex-row flex-grow justify-end">
+                <ul className="flex flex-row list-none space-x-10 items-center">
+                    <li>
+                        <Link to={"/journeys"}>Journeys</Link>
+                    </li>
+                    <li>
+                        <Link to={"/journeys/search"}>Search</Link>
+                    </li>
+                    <li>
+                        <Link to={"/journeys/982573"}>(Details)</Link>
+                    </li>
+                    <li>
+                        Sign Up
+                    </li>
+                    <li>
+                        <Button>Log In</Button>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
